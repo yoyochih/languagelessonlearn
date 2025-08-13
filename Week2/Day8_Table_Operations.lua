3fee0032814d2395ba7723ca919e183e3239ded0
Jerry Yu <yu_jerry@apple.com>
2025-08-13 13:20:22 +0800

    更新Week2/Day8表格操作练习代码

--- Week2/Day8_Table_Operations.lua
+++ Week2/Day8_Table_Operations.lua
@@ -6,10 +6,13 @@ print("Hello from Day8_Table_Operations")
 
 -- 練習：
 -- 1. 排序數列
--- 在此撰寫你的程式碼
+-- local t={{5,1,4,2}}; table.sort(t); for i=1,#t do print(t[i]) end
+local t={{5,1,4,2}}; table.sort(t); for i=1,#t do print(t[i]) end
 
 -- 2. 淺拷貝
--- 在此撰寫你的程式碼
+-- local function clone(a) local b={{}}; for k,v in pairs(a) do b[k]=v end; return b end; local x={{a=1}}; local y=clone(x); print(y.a)
+
 
 -- 3. 移除元素
--- 在此撰寫你的程式碼
+-- local t={{"x","y","z"}}; table.remove(t,2); for i=1,#t do print(t[i]) end
+
