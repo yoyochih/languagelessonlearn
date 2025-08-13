a70ee9a6496f98f0a3d987bbfa81f648d27e6bd1
Jerry Yu <yu_jerry@apple.com>
2025-08-13 14:07:57 +0800

    更新Week2/Day9表格操作练习代码完成

--- Week2/Day9_Iterators.lua
+++ Week2/Day9_Iterators.lua
@@ -6,10 +6,18 @@ print("Hello from Day9_Iterators")
 
 -- 練習：
 -- 1. 比較 pairs 與 ipairs
--- 在此撰寫你的程式碼
+-- local t={{10,20,a=30}}; for k,v in pairs(t) do print("pairs",k,v) end; for i,v in ipairs(t) do print("ipairs",i,v) end
+local t={10,20,a=30}; for k,v in pairs(t) do print("pairs",k,v) end; for i,v in ipairs(t) do print("ipairs",i,v) end
+
 
 -- 2. 自訂 from..to 迭代器
--- 在此撰寫你的程式碼
+-- local function fromto(a,b) local i=a-1; return function() i=i+1; if i<=b then return i end end end;
+--for v in fromto(3,6) do print(v) end
+ local function fromto(a,b) local i=a-1; return function() i=i+1; if i<=b then return i end end end;
+  for v in fromto(3,6) do print(v) end
+
 
 -- 3. 逐字元迭代
--- 在此撰寫你的程式碼
+-- local s="lua"; local i=0; local function chars() i=i+1; return s:sub(i,i) ~= "" and s:sub(i,i) or nil end; for c in chars do print(c) end
+local s="lua"; local i=0; local function chars() i=i+1; return s:sub(i,i) ~= "" and s:sub(i,i) or nil end; for c in chars do print(c) end
+
